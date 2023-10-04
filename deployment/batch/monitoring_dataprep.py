0b4bd8d17c8280c99c3340679ae49e1815e6a22a
Maria Medina <mamedina@microsoft.com>
2023-10-04 13:04:42 +0200

    Process input data

--- deployment/batch/monitoring_dataprep.py
+++ deployment/batch/monitoring_dataprep.py
@@ -3,9 +3,9 @@
 import os
 import argparse
 import pandas as pd
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, functions as f
 
-# import mltable
+import mltable
 # import tempfile
 # from azureml.fsspec import AzureMachineLearningFileSystem
 # from datetime import datetime
@@ -15,70 +15,52 @@ from pyspark.sql import SparkSession
 def main(data_window_start, data_window_end, input_data_uri, output_data_uri):
     print("main")
 
-    spark = SparkSession.builder.appName("AccessParquetFiles").getOrCreate()
-    print(spark)
+    # Read batch predictions data
+    input_preds_sdf = read_input_predictions(input_data_uri)
+    print(f"Total rows processed: {input_preds_sdf.count()}")
+    print("Schema:")
+    print(input_preds_sdf, "\n")
 
-    try:
-        df = spark.read.mltable(input_data_uri)
-    except Exception as ex:
-        print("mltable - no")
-        print(ex)
+    # Window start/end in ISO8601 format, example: '2023-10-03T13:12:52.037534Z'
+    # Simulate like it's 2016 - same as use case data
+    # Take date only bc predictions are scheduled daily - #TODO configure in monitor directly
+    custom_window_start = f'2016-{data_window_start[5:10]} 00:00:00'
+    custom_window_end = f'2016-{data_window_end[5:10]} 00:00:00'
+    print(f'Window start: {data_window_start} => {custom_window_start}')
+    print(f'Window end: {data_window_end} => {custom_window_end}')
 
-    print("++++++++")
+    # Filter window
+    output_sdf = (input_preds_sdf
+        .filter(f.col('timestamp') >= custom_window_start)
+        .filter(f.col('timestamp') <= custom_window_end)
+        .select('prediction')
+    )
+    print(f"Rows after filtering: {output_sdf.count()}")
+    print("Schema:")
+    print(output_sdf, "\n")
 
-    try:
-        df = spark.read.json(input_data_uri)
-        print(df)
-    except Exception as ex:
-        print("json - no")
-        print(ex)
-
-    print("++++++++")
-
-    try:
-        df = mltable.from_json_lines_files(input_data_uri, invalid_lines='error', include_path_column=True)
-        print(df)
-    except Exception as ex:
-        print("mltable json - no")
-        print(ex)
-
-    print("++++++++")
-
-    try:
-        df = mltable.from_json_lines_files(paths=[{"pattern": f"{input_data_uri}**/*.csv"}], invalid_lines='error', include_path_column=True)
-        print(df)
-    except Exception as ex:
-        print("mltable json vpattern - no")
-        print(ex)
-
-    print("++++++++")
-
-    try:
-        df = mltable.from_paths(input_data_uri, invalid_lines='error', include_path_column=True)
-        print(df)
-    except:
-        print("mltable paths - no")
-        print(ex)
-
-    print("++++++++")
-
-    try:
-        df = mltable.from_paths(paths=[{"pattern": f"{input_data_uri}**/*.csv"}], invalid_lines='error', include_path_column=True)
-        print(df)
-    except:
-        print("mltable paths - no")
-        print(ex)
-
-    print("++++++++")
-
-    output_df = spark.createDataFrame(pd.DataFrame([], index=[0], columns=["prediction"]))
-    output_df.write.option("output_format", "parquet").option("overwrite", True).mltable(output_data_uri)
-
-    print("++++OUT++++")
-    print(output_df)
-    print("++++++++")
+    # output_sdf.write.option('output_format', 'parquet').option('overwrite', True).mltable(output_data_uri)
+    output_sdf.write.format('parquet').option('overwrite', True).mltable(output_data_uri)
 
     print("Finished.")
+
+
+def read_input_predictions(input_data_uri):
+
+    preds_sdf = (spark
+        .read.format('text')
+        .load(f'{input_data_uri}/*/*/*/predictions.csv')
+        .withColumn('value', f.from_json(f.col('value'), schema='array<string>'))
+        .withColumn('filepath', f.input_file_name())
+        .withColumn('dirpath', f.regexp_extract(f.col('filepath'), f'{input_data_uri}(.*)/predictions.csv', 1))
+        .select(
+            f.to_timestamp(f.col('dirpath'), format='yyyy/MM/dd').alias('timestamp'),
+            f.col('value')[0].alias('inputfile'),
+            f.col('value')[1].astype('float').alias('prediction')
+        )
+    )
+
+    return preds_sdf
 
 
 def parse_args(args_list=None):
@@ -101,6 +83,13 @@ if __name__ == '__main__':
     print("----------")
     print(os.listdir())
     print("----------")
+
+    import pyspark
+    print(">>> pyspark version:")
+    print(pyspark.__version__)
+
+    spark = SparkSession.builder.appName("monitoringdataprep").getOrCreate()
+    print(spark)
 
     main(
         data_window_start=args.data_window_start,
